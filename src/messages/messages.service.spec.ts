b8df3008fe28dd63d62fccf2f5581294e41735dd
kreuzerk <kevin.kreuzer90@icloud.com>
2017-06-15 08:16:15 +0200

    kk: Test message service

--- src/messages/messages.service.spec.ts
+++ src/messages/messages.service.spec.ts
@@ -20,14 +20,78 @@ describe('Message Service', () => {
             summary: messageSummary,
             detail: messageContent
         };
-
         sut.getMessageStream()
             .subscribe(message => {
                 expect(message).toEqual(expectedMessage);
                 done();
             });
-
         // when
         sut.createSuccessMessage(messageContent, messageSummary);
+    });
+
+    it('should create a errormessage and stream it into the message subject', done => {
+        // given
+        const sut = TestBed.get(MessagesService);
+        const messageContent = 'Awful Error';
+        const messageSummary = 'Error';
+        const expectedMessage = {
+            severity: 'error',
+            summary: messageSummary,
+            detail: messageContent
+        };
+        sut.getMessageStream()
+            .subscribe(message => {
+                expect(message).toEqual(expectedMessage);
+                done();
+            });
+        // when
+        sut.createErrorMessage(messageContent, messageSummary);
+    });
+
+    it('should create a infomessage and stream it into the message subject', done => {
+        // given
+        const sut = TestBed.get(MessagesService);
+        const messageContent = 'Super important information';
+        const messageSummary = 'Information';
+        const expectedMessage = {
+            severity: 'info',
+            summary: messageSummary,
+            detail: messageContent
+        };
+        sut.getMessageStream()
+            .subscribe(message => {
+                expect(message).toEqual(expectedMessage);
+                done();
+            });
+        // when
+        sut.createInfoMessage(messageContent, messageSummary);
+    });
+
+    it('should create a warningmessage and stream it into the message subject', done => {
+        // given
+        const sut = TestBed.get(MessagesService);
+        const messageContent = 'Super important warning';
+        const messageSummary = 'Warning';
+        const expectedMessage = {
+            severity: 'warn',
+            summary: messageSummary,
+            detail: messageContent
+        };
+        sut.getMessageStream()
+            .subscribe(message => {
+                expect(message).toEqual(expectedMessage);
+                done();
+            });
+        // when
+        sut.createWarningMessage(messageContent, messageSummary);
+    });
+
+    it('should stream a new message in the clearStream when calling clearMessages', done => {
+        // given
+        const sut = TestBed.get(MessagesService);
+        sut.getCancelStream()
+            .subscribe(e => done());
+        // when
+        sut.clearMessages();
     });
 });
