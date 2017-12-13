e3d6e89b585d9baf9df08c43bd08913514c04ac2
kreuzerk <kevin.kreuzer90@icloud.com>
2017-12-13 15:20:19 +0100

    Add getAuthHeader Method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,52 +1,53 @@
-import { Injectable } from '@angular/core';
-import { KeycloakInstance, KeycloakProfile } from 'keycloak-js';
-import { Observable } from 'rxjs/Observable';
-import { fromPromise } from 'rxjs/observable/fromPromise';
-import { of } from 'rxjs/observable/of';
+import {Injectable} from '@angular/core';
+import {KeycloakInstance, KeycloakProfile} from 'keycloak-js';
+import {Observable} from 'rxjs/Observable';
+import {fromPromise} from 'rxjs/observable/fromPromise';
+import {of} from 'rxjs/observable/of';
 
 @Injectable()
 export class AuthService {
 
-  keycloak: KeycloakInstance;
+    keycloak: KeycloakInstance;
 
-  login(): void {
-    this.keycloak.login();
-  }
-
-  logout(): void {
-    this.keycloak.logout();
-  }
-
-  authenticated(): boolean {
-    return this.keycloak.authenticated;
-  }
-
-  refreshToken(minValidity: number = 5): Promise<boolean> {
-    return new Promise((resolve, reject) => {
-      this.keycloak.updateToken(minValidity)
-        .success(resolve)
-        .error(reject);
-    });
-  }
-
-  /**
-   * Can be used in a request header
-   * Authorization: `Bearer ${authService.getToken()}`
-   */
-  getToken(): string {
-    return this.keycloak.token;
-  }
-
-  getUserInfo(): Observable<KeycloakProfile | undefined> {
-    if (!this.authenticated() || this.keycloak.profile) {
-      return of(this.keycloak.profile);
+    login(): void {
+        this.keycloak.login();
     }
 
-    return fromPromise(new Promise((resolve, reject) => {
-      this.keycloak.loadUserProfile()
-        .success(resolve)
-        .error(err => reject(err));
-    }));
-  }
+    logout(): void {
+        this.keycloak.logout();
+    }
 
+    authenticated(): boolean {
+        return this.keycloak.authenticated;
+    }
+
+    refreshToken(minValidity: number = 5): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            this.keycloak.updateToken(minValidity)
+                .success(resolve)
+                .error(reject);
+        });
+    }
+
+    getToken(): string {
+        return this.keycloak.token;
+    }
+
+    public getAuthHeader(): any {
+        return {
+            'Authorization': 'Bearer ' + this.getToken()
+        };
+    }
+
+    getUserInfo(): Observable<KeycloakProfile | undefined> {
+        if (!this.authenticated() || this.keycloak.profile) {
+            return of(this.keycloak.profile);
+        }
+
+        return fromPromise(new Promise((resolve, reject) => {
+            this.keycloak.loadUserProfile()
+                .success(resolve)
+                .error(err => reject(err));
+        }));
+    }
 }
