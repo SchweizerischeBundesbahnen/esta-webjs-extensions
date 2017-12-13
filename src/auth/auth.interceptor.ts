df48a7f5c57063fc3ea1d09f7b210f1bad59cfe3
kreuzerk <kevin.kreuzer90@icloud.com>
2017-12-13 15:31:43 +0100

    Add authInterceptor tests

--- src/auth/auth.interceptor.ts
+++ src/auth/auth.interceptor.ts
@@ -1,48 +1,48 @@
 import {
-  HTTP_INTERCEPTORS,
-  HttpErrorResponse,
-  HttpEvent,
-  HttpHandler,
-  HttpInterceptor,
-  HttpRequest
+    HTTP_INTERCEPTORS,
+    HttpErrorResponse,
+    HttpEvent,
+    HttpHandler,
+    HttpInterceptor,
+    HttpRequest
 } from '@angular/common/http';
 import {ClassProvider, Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
+import {ErrorObservable} from 'rxjs/observable/ErrorObservable';
 import {catchError} from 'rxjs/operators';
 
 import {AuthService} from './auth.service';
 
-enum HTTP_STATUS_CODE {
-  UNAUTHORIZED = 401,
-  FORBIDDEN = 403
+export enum HTTP_STATUS_CODE {
+    UNAUTHORIZED = 401,
+    FORBIDDEN = 403
 }
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private authService: AuthService) {
-  }
-
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (!this.authService.authenticated) {
-      return next.handle(req);
+    constructor(private authService: AuthService) {
     }
 
-    return next.handle(req).pipe(catchError((error: any) => this.handleErrorResponses(error)));
-  }
-
-  private handleErrorResponses(error: any): Observable<any> {
-    if (error instanceof HttpErrorResponse) {
-      if (error.status === HTTP_STATUS_CODE.UNAUTHORIZED || error.status === HTTP_STATUS_CODE.FORBIDDEN) {
-        this.authService.login();
-      }
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        if (!this.authService.authenticated) {
+            return next.handle(req);
+        }
+        return next.handle(req).pipe(catchError((error: any) => this.handleErrorResponses(error)));
     }
-    return Observable.throw(error);
-  }
+
+    private handleErrorResponses(error: any): Observable<any> {
+        if (error instanceof HttpErrorResponse) {
+            if (error.status === HTTP_STATUS_CODE.UNAUTHORIZED || error.status === HTTP_STATUS_CODE.FORBIDDEN) {
+                this.authService.login();
+            }
+        }
+        return ErrorObservable.create(error);
+    }
 }
 
 export const AUTH_INTERCEPTOR: ClassProvider = {
-  provide: HTTP_INTERCEPTORS,
-  useClass: AuthInterceptor,
-  multi: true,
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true,
 };
--- src/auth/keycloak-config.ts
+++ src/auth/keycloak-config.ts
@@ -1,6 +1,6 @@
 export interface KeycloakConfig {
-  url?: string;
-  realm?: string;
-  clientId?: string;
-  credentials?: { secret: string };
+    url?: string;
+    realm?: string;
+    clientId?: string;
+    credentials?: { secret: string };
 }
